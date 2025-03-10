f29f907e3d5bf113108cdf99d0ae25e0f5769e3b
Prasad Arun Dhobale <dhobaleprasad3@gmail.com>
2025-03-10 15:39:28 +0530

    changes made

--- lambda.js
+++ lambda.js
@@ -4,7 +4,7 @@ const serverless = require('serverless-http');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ message: "Hello, Lambda!" });
+    res.json({ message: "Hello, Lambda!..." });
 });
 
 module.exports.handler = serverless(app);
